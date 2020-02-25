54110f19024b4a7f1f9444d46e5ceb4d4b1ba86b
Jeff Acquaviva <jracquaviva@misi.tech>
2020-02-25 11:20:09 -0500

    renamed driver jar file

--- GM3/main.py
+++ GM3/main.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+This is an example driver file to call GRASSMARLIN as a plugin for other modules
+"""
 
 import argparse
 import subprocess
@@ -6,7 +9,7 @@ import subprocess
 def main(args):
     # java_path = "/usr/lib/jvm/java-11-openjdk-amd64/bin/java"
     java_path = args.java
-    jar_name = "target/minow-1.0-SNAPSHOT-jar-with-dependencies.jar"
+    jar_name = "target/grassmarlin-cli-1.0-SNAPSHOT-jar-with-dependencies.jar"
     fingerprint_dir = args.fingerprints
     pcap_file = args.pcap
 
